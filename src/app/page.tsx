6cb75aaf3630756735c9fe81916d588848fd923a
ErTeamDeDama <erteamdedama@gmail.com>
2025-05-14 12:43:37 +0200

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 
 import Link from 'next/link';
+import styles from './global.css';
 export default function Home() {
   return (
     <div>
       <div>
         
       </div>
-      <body>
+      <body className={styles.body}>
         <h1>Benvenuto nella Home</h1>
         <Link href="/admin">Vai alla pagina Admin</Link>
       </body>
